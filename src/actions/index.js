e510f130fd7d5c4048321f0a97dd53784eb8c183
Ben Mercer <ben@henroben.co.uk>
2017-01-30 16:28:28 +0000

    S5:L58 - axios and redux-promise, ajax call to api

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = "ff5c632a1ce5af01b568a76462afa988";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},uk`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
