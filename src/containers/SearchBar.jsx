f30f4e6bc33a5d17050d03f628ec6d51b937585f
Ben Mercer <ben@henroben.co.uk>
2017-01-30 14:08:29 +0000

    S5:L53 - Controlled components and bound context

--- src/containers/SearchBar.jsx
+++ src/containers/SearchBar.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value });
+    }
+
     render() {
         return(
             <form className="input-group">
-                <input className="form-control input-lg" />
+                <input className="form-control input-lg"
+                    placeholder="Get a five-day forecast in your favourite cities"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button className="btn btn-secondary" type="submit">Submit</button>
                 </span>
